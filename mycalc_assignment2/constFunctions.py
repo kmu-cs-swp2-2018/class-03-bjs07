793983e2777697c1854e3a6108d9da0cbbfca35d
bjs <38303324+bjs07@users.noreply.github.com>
2018-10-25 07:05:35 +0900

    Add files via upload

---
+++ mycalc_assignment2/constFunctions.py
@@ -0,0 +1,30 @@
+
+def pi(numStr):
+    try:
+        r='3.141592'
+    except:
+        r='Error!'
+
+    return r
+
+def light(numStr):
+    try:
+        r='3E+8'
+    except:
+        r='Error!'
+    return r
+
+def sound(numStr):
+    try:
+        r='340'
+    except:
+        r='Error!'
+    return r
+
+def sun(numStr):
+    try:
+        r='1.5E+8'
+    except:
+        r='Error!'
+    return r
+
---
+++ mycalc_assignment2/constants.py
@@ -0,0 +1,11 @@
+from constFunctions import *
+constantMap=[
+    ('pi',pi),
+    ('빛의 이동 속도 (m/s)',light),
+    ('소리의 이동 속도 (m/s)',sound),
+    ('태양과의 평균 거리 (km)',sun),
+
+]
+
+constantList=[x[0] for x in constantMap]
+
