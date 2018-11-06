fd87082b15df44423ba1c981552b667df4c3438c
bjs <38303324+bjs07@users.noreply.github.com>
2018-11-06 11:03:51 +0900

    Update guess.py

--- 10-2/guess.py
+++ 10-2/guess.py
@@ -3,38 +3,30 @@ class Guess:
     def __init__(self, word):
         self.numTries=0 #시도횟수
         self.guessedChars=[] #찾아본 단어
-        self.answer = word #정답 단어
-        self.secretWord='_'*len(word)
-        self.currentStatus=''
-        self.temp=word
+        self.secretWord=word
+        self.currentStatus='_'*len(word)
 
     def display(self): #칸 나타내기
-        print(self.secretWord)
+        print(self.currentStatus)
         print("Tries: %d" %self.numTries)
-        print(self.answer)
+        print(self.secretWord)
         print(self.guessedChars)
 
     def guess(self, character):
 
         #주어진 글자를 사용한 글자에 추가
         self.guessedChars.append(character)
-        self.currentStatus+=character
         #비밀단어에 주어진 글자가 없으면 틀린 횟수 증가
-        '''if character in self.answer:
-            for i in range(0,len(self.answer)):
-                if character == self.answer[i]:
-                    self.secretWord[i]=character
-        '''
+        if character in self.secretWord:
+            for n in range(len(self.secretWord)):
+                if character==self.secretWord[n]:
+                    self.currentStatus=self.currentStatus[:n]+self.secretWord[n]+self.currentStatus[n+1:]
 
-        if character in self.answer:
-
-            n=self.answer.index(character) #단어 인덱스
-            self.secretWord=self.secretWord[:n]+self.answer[n]+self.secretWord[n+1:]
-            self.numTries-=1
         # 없으면 틀린 횟수 증가
-        self.numTries +=1
+        else:
+            self.numTries +=1
 
-        if self.secretWord ==self.answer:
+        if self.currentStatus ==self.secretWord:
             return True
         else:
             return False
