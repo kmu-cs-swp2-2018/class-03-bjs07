81bf3938190d0e1558779286db97ba91fe4523b6
bjs <38303324+bjs07@users.noreply.github.com>
2018-11-06 11:43:21 +0900

    Update game.py

--- 10-2/game.py
+++ 10-2/game.py
@@ -1,6 +1,7 @@
 from hangman import Hangman
 from guess import Guess
 from word import Word
+#알파벳 안에 있는지 검사하기 위한 전역변수
 global alphabet
 alphabet='abcdefghijklmnopqrstuvwxyz'
 
@@ -19,16 +20,17 @@ def gameMain():
         display = hangman.get(maxTries - guess.numTries)
         print(display)
         guess.display()
-
+        
+        #lstrip을 넣어서 공백은 자동으로 제거
         guessedChar = input('Select a letter: ')
-        if len(guessedChar) != 1:
+        if len(guessedChar.lstrip()) != 1:
             print('One character at a time!')
             continue
-        if guessedChar in guess.guessedChars:
+        if guessedChar.lstrip() in guess.guessedChars:
             print('You already guessed \"' + guessedChar + '\"')
             continue
-
-        if guessedChar not in alphabet:
+        # 입력한 문자가 알파벳 소문자가 아니면 오류
+        if guessedChar.lstrip() not in alphabet:
             print('Character must be English small letter!')
             continue
 
